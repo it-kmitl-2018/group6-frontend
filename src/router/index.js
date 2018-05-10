8efdcfb3adb5ee40ba71f8df15a1cb6fbc3e300b
pongsakorn khemanitthathai <it58070087@office365.it.kmitl.ac.th>
2018-05-10 21:49:59 +0700

    edit router
    edit router name payment Information

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import paymentInformation from '@/components/payment Information'
+import paymentInformation from '@/components/Payment Information'
 import buyerForm from '@/components/buyerForm'
 
 Vue.use(Router)
