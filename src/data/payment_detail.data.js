dd305921a1aef141a4c1f2379826e9b7385a6ecc
pongsakorn khemanitthathai <it58070087@office365.it.kmitl.ac.th>
2018-05-11 15:51:58 +0700

    add payment_detail Json
    add payment information in Json for send data Object to backend

---
+++ src/data/payment_detail.data.js
@@ -0,0 +1,20 @@
+export default {
+    data: {
+        delivery_detail: {
+            currency_code: '',
+            tax_type_code: '',
+            tax_rate: '',
+            basis_amount: '',
+            is_charged: '',
+            charge_amount: '',
+            charge_type_code: '',
+            payment_term_type_code: '',
+            original_amount: '',
+            total_amount: '',
+            difference_sale_amount: '',
+            total_allowance_amount: '',
+            total_tax_amount: '',
+            grand_total_amount: ''
+        }
+    }
+}
