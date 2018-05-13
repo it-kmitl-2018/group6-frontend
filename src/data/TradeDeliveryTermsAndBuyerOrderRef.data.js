03f573b07efef544f5667dbb1501f832fdb1d7c3
yuureihunter <58070101@kmitl.ac.th>
2018-05-13 20:00:11 +0700

    edit indent data file

--- src/data/TradeDeliveryTermsAndBuyerOrderRef.data.js
+++ src/data/TradeDeliveryTermsAndBuyerOrderRef.data.js
@@ -1,15 +1,15 @@
 export default{
-    data: {
-        delivery_type_code: '',
-        order_reference: {
-            id: '',
-            created_date: '',
-            reference_type_coder: ''
-        },
-        additional_reference: {
-            id: '',
-            created_date: '',
-            reference_type_code_additional: ''
-        } 
-    }
+  data: {
+    delivery_type_code: '',
+    order_reference: {
+      id: '',
+      created_date: '',
+      reference_type_coder: ''
+    },
+    additional_reference: {
+      id: '',
+      created_date: '',
+      reference_type_code_additional: ''
+    } 
+  }
 }
