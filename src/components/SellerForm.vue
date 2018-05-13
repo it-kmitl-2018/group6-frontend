c5a860be39ca3585adc24465b9900ae5d1f9960a
yuureihunter <58070101@kmitl.ac.th>
2018-05-13 18:19:52 +0700

    edit indent

--- src/components/SellerForm.vue
+++ src/components/SellerForm.vue
@@ -1,158 +1,158 @@
 <template>
-    <div class="sellerForm">
-        <h1>{{ title }}</h1>
-        <div style="padding:10px; border: 1px solid #eeeeee; background-color: #FFFFF0;">
-            <h2 style="display: inline">ข้อตกลงทางการค้า</h2><br>
-            <table border="0" cellspacing="0px"  style="width: 500px">
-                <tbody>
-                    <tr>
-                        <td>
-                            <h3 style="display: inline">ผู้ขาย</h3>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;รหัสผู้ค้า (ผู้ขาย) </td>
-                        <td>
-                            <input type="text" maxlength="35" v-model.lazy="disc.id_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;รหัสผู้ค้าสากล (ผู้ขาย) </td>
-                        <td>
-                            <input type="text" maxlength="35" v-model.lazy="disc.global_id_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;ชื่อผู้ขาย </td>
-                        <td>
-                            <input type="text" maxlength="140" v-model.lazy="disc.name_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <h4 style="display: inline">ข้อมูลนิติบุคคล</h4>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;เลขประจาตัวผู้เสียภาษีอากร (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="35" v-model.lazy="disc.id_specified_seller">
-                        </td>
-                    </tr>
+  <div class="sellerForm">
+    <h1>{{ title }}</h1>
+    <div style="padding:10px; border: 1px solid #eeeeee; background-color: #FFFFF0;">
+      <h2 style="display: inline">ข้อตกลงทางการค้า</h2><br>
+      <table border="0" cellspacing="0px"  style="width: 500px">
+        <tbody>
+          <tr>
+            <td>
+              <h3 style="display: inline">ผู้ขาย</h3>
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;รหัสผู้ค้า (ผู้ขาย) </td>
+            <td>
+              <input type="text" maxlength="35" v-model.lazy="disc.id_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;รหัสผู้ค้าสากล (ผู้ขาย) </td>
+            <td>
+              <input type="text" maxlength="35" v-model.lazy="disc.global_id_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;ชื่อผู้ขาย </td>
+            <td>
+              <input type="text" maxlength="140" v-model.lazy="disc.name_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h4 style="display: inline">ข้อมูลนิติบุคคล</h4>
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;เลขประจาตัวผู้เสียภาษีอากร (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="35" v-model.lazy="disc.id_specified_seller">
+            </td>
+          </tr>
 
-                    <tr>
-                        <td>
-                            <h4 style="display: inline">รายละเอียดการติดต่อ :<br> การติดต่อทางอีเมล</h4>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;URI (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="256" v-model.lazy="disc.uri_id_email_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;อีเมล (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="2048" v-model.lazy="disc.complete_number_email_seller">
-                        </td>
-                    </tr>
+          <tr>
+            <td>
+              <h4 style="display: inline">รายละเอียดการติดต่อ :<br> การติดต่อทางอีเมล</h4>
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;URI (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="256" v-model.lazy="disc.uri_id_email_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;อีเมล (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="2048" v-model.lazy="disc.complete_number_email_seller">
+            </td>
+          </tr>
 
-                    <tr>
-                        <td>
-                            <h4 style="display: inline">ที่อยู่ผู้ขาย</h4>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;รหัสไปรษณีย์ (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="16" v-model.lazy="disc.post_code_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;ชื่ออาอาคาร (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="70" v-model.lazy="disc.building_name_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;ซอย (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="70" v-model.lazy="disc.line_3_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;หมู่บ้าน (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="70" v-model.lazy="line_4_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;หมู่ที่ (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="70" v-model.lazy="disc.line_5_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;ถนน (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="70" v-model.lazy="disc.street_name_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;รหัสอำเภอ (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" v-model.lazy="city_name_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;รหัสตำบล (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" v-model.lazy="disc.city_sub_divition_name_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;รหัสประเทศ (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" v-model.lazy="disc.country_id_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;รหัสจังหวัด (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" id="disc.country_sub_divition_id_trade_address_seller">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;บ้านเลขที่ (ผู้ขาย)</td>
-                        <td>
-                            <input type="text" maxlength="16" v-model.lazy="disc.building_number_trade_address_seller">
-                        </td>
-                    </tr>
+          <tr>
+            <td>
+              <h4 style="display: inline">ที่อยู่ผู้ขาย</h4>
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;รหัสไปรษณีย์ (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="16" v-model.lazy="disc.post_code_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;ชื่ออาอาคาร (ผู้ขาย)</td>
+            <td>
+               <input type="text" maxlength="70" v-model.lazy="disc.building_name_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;ซอย (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="70" v-model.lazy="disc.line_3_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;หมู่บ้าน (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="70" v-model.lazy="line_4_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;หมู่ที่ (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="70" v-model.lazy="disc.line_5_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;ถนน (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="70" v-model.lazy="disc.street_name_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;รหัสอำเภอ (ผู้ขาย)</td>
+            <td>
+              <input type="text" v-model.lazy="city_name_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;รหัสตำบล (ผู้ขาย)</td>
+            <td>
+              <input type="text" v-model.lazy="disc.city_sub_divition_name_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;รหัสประเทศ (ผู้ขาย)</td>
+            <td>
+              <input type="text" v-model.lazy="disc.country_id_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;รหัสจังหวัด (ผู้ขาย)</td>
+            <td>
+              <input type="text" id="disc.country_sub_divition_id_trade_address_seller">
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;บ้านเลขที่ (ผู้ขาย)</td>
+            <td>
+              <input type="text" maxlength="16" v-model.lazy="disc.building_number_trade_address_seller">
+            </td>
+          </tr>
 
-                    <tr>
-                        <td> 
-                            <button v-on:click="submit()">หน้าถัดไป</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+          <tr>
+            <td> 
+              <button v-on:click="submit()">หน้าถัดไป</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'sellerForm',
-        data () {
-            return{
-                title: 'ใบกำกับภาษี',
-            }
-        },
-        methods: {
-            submit: function(){
-                console.log(this.disc);
-            }
-        }
+export default {
+  name: 'sellerForm',
+  data () {
+    return{
+      title: 'ใบกำกับภาษี',
     }
+  },
+  methods: {
+    submit: function(){
+      console.log(this.disc);
+    }
+  }
+}
 </script>
