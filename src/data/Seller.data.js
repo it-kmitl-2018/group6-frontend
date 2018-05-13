501e9e906e68f2c5dc984793f8b0689a0ce78b03
yuureihunter <58070101@kmitl.ac.th>
2018-05-13 20:02:41 +0700

    edit indent data file

--- src/data/Seller.data.js
+++ src/data/Seller.data.js
@@ -1,21 +1,21 @@
 export default{
-    data: {
-        id_seller: '',
-        global_id_seller: '',
-        name_seller: '',
-        id_specified_seller: '',
-        uri_id_email_seller: '',
-        complete_number_email_seller: '',
-        post_code_address_seller: '',
-        building_name_trade_address_seller: '',
-        line_3_trade_address_seller: '',
-        line_4_trade_address_seller: '',
-        line_5_trade_address_seller: '',
-        street_name_trade_address_seller: '',
-        city_name_trade_address_seller: '',
-        city_sub_divition_name_trade_address_seller: '',
-        country_id_trade_address_seller: '',
-        country_sub_divition_id_trade_address_seller: '',
-        building_number_trade_address_seller: ''
-    }
+  data: {
+    id_seller: '',
+    global_id_seller: '',
+    name_seller: '',
+    id_specified_seller: '',
+    uri_id_email_seller: '',
+    complete_number_email_seller: '',
+    post_code_address_seller: '',
+    building_name_trade_address_seller: '',
+    line_3_trade_address_seller: '',
+    line_4_trade_address_seller: '',
+    line_5_trade_address_seller: '',
+    street_name_trade_address_seller: '',
+    city_name_trade_address_seller: '',
+    city_sub_divition_name_trade_address_seller: '',
+    country_id_trade_address_seller: '',
+    country_sub_divition_id_trade_address_seller: '',
+    building_number_trade_address_seller: ''
+  }
 }
