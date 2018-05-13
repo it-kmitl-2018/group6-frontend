3684f7f02d974f967060366910adfe81af008f32
pongsakorn khemanitthathai <it58070087@office365.it.kmitl.ac.th>
2018-05-13 16:50:17 +0700

    Create supplyChainDeliverySender Js
    Create New supplyChainDeliverySender. Using Js. to Send data to Backend

---
+++ src/data/supplyChainDeliverySender.data.js
@@ -0,0 +1,12 @@
+export default {
+    data: {
+        delivery_detail: {
+            sender: {
+                sender_id: '',
+                global_id: '',
+                name: '',
+                delivery_date: ''
+            }
+        }
+    }
+}
