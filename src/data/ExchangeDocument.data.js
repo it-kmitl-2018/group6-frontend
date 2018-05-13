cb3dc6345db61933800ceda2ef7c3af2c1629b42
yuureihunter <58070101@kmitl.ac.th>
2018-05-13 20:03:55 +0700

    edit indent data file

--- src/data/ExchangeDocument.data.js
+++ src/data/ExchangeDocument.data.js
@@ -1,16 +1,16 @@
 export default{
-    data: {
-        id_guideline: '',
-        header: {
-            id_document: '',
-            name_document: '',
-            type_code_document: '',
-            issue_date_time_document: '',
-            purpose_document: '',
-            purpose_code_document: '',
-            global_id_document: '',
-            creation_date_time_document: '',
-            remark_include_note: ''
-        }
+  data: {
+    id_guideline: '',
+    header: {
+      id_document: '',
+      name_document: '',
+      type_code_document: '',
+      issue_date_time_document: '',
+      purpose_document: '',
+      purpose_code_document: '',
+      global_id_document: '',
+      creation_date_time_document: '',
+      remark_include_note: ''
     }
+  }
 }
