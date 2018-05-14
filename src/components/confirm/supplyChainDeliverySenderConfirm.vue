3b56e70fbb09b1f28d5956fb0f98b1bb0568d678
pongsakorn khemanitthathai <it58070087@office365.it.kmitl.ac.th>
2018-05-14 15:37:58 +0700

    Create supply Chain Delivery Sender Confirm form
    Create supply Chain Delivery Sender Confirm page Using span. to can Confirm what is user input to form.

---
+++ src/components/confirm/supplyChainDeliverySenderConfirm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="supplyChainDeliverySenderConfirm">
+    <h2 style="display: inline">ข้อมูลการจัดส่งทางการค้า</h2><br><br>
+    <h4 style="display: inline">ข้อมูลผู้ส่ง</h4><br>
+    &nbsp;รหัสผู้ส่ง : <span>{{ supplyChainDeliverySenderVue.sender_id }}</span><br>
+    &nbsp;รหัสผู้ค้าสากล (ผู้ส่ง) : <span>{{ supplyChainDeliverySenderVue.global_id }}</span><br>
+    &nbsp;ชื่อผู้ส่ง : <span>{{ supplyChainDeliverySenderVue.name }}</span><br>
+    <h4 style="display: inline">การนัดส่งสินค้า</h4><br>
+    &nbsp;วันเวลานัดส่งสินค้า : <span>{{ supplyChainDeliverySenderVue.delivery_date }}</span><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['supplyChainDeliverySenderConfirmVue']
+}
+</script>
